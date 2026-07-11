aa02e5e9c4266a9b8daa908adf5d44fc0bafc14e
apagyizsolt6-ship-it <apagyizsolt6@gmail.com>
2026-07-11 23:19:03 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'services/sports_service.dart';
 import 'models/league_model.dart';
+import 'screens/details_screen.dart';
 
 void main() => runApp(const SportPulseApp());
 
@@ -10,6 +11,7 @@ class SportPulseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: const Color(0xFF020C1B),
         cardColor: const Color(0xFF112240),
@@ -47,17 +49,27 @@ class _LeagueListScreenState extends State<LeagueListScreen> {
             return ListView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
+                final league = snapshot.data![index];
                 return Card(
-                  margin: const EdgeInsets.all(8),
+                  margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
                   child: ListTile(
-                    title: Text(snapshot.data![index].name),
-                    subtitle: Text(snapshot.data![index].sport),
+                    title: Text(league.name),
+                    subtitle: Text(league.sport),
+                    trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailsScreen(league: league),
+                        ),
+                      );
+                    },
                   ),
                 );
               },
             );
           } else if (snapshot.hasError) {
-            return Center(child: Text("${snapshot.error}"));
+            return Center(child: Text("Hiba: ${snapshot.error}"));
           }
           return const Center(child: CircularProgressIndicator());
         },
